ae4760434386bce1d3fea61dc1d42d46cb5f18d2
ahtec <gerarddoets@gmail.com>
2017-10-25 18:31:52 +0200

    nu met kege;oppervlakte

--- weekopdracht1.php
+++ weekopdracht1.php
@@ -13,8 +13,7 @@ $bewerking = $_GET['bewerking'];
 
 $eerste = $_GET['eerste'];
 $tweede = $_GET['tweede'];
-// 10 tot de 3e is 1000  dus 10log1000 = 3  
-
+// 10 tot de 3e is 1000  dus 10log1000 == 3  
 //echo log(1000, 10);
 
 echo "<br>";
@@ -44,10 +43,15 @@ switch ($bewerking) {
     case "logaritme":
         logaritme($eerste, $tweede);
         break;
+    case "kegeloppervlakte":
+        kegeloppervlakte($eerste, $tweede);
+        break;
+
+
     default:
         echo "Onbekende bewerking: " . $bewerking;
         echo "<br>U kunt gebruiken: optellen aftrekken vermenigvuldigen delen machtverheffen wortel";
-        echo" rest logaritme";
+        echo" rest logaritme kegeloppervlakte";
         break;
 }
 echo "<br>";
@@ -91,7 +95,7 @@ function rest($var1, $var2) {
     if ($var2 <= 0 || $var1 <= 0) {
         echo "Beide  argumenten moeten  positef zijn voor modulo berekening";
     } else {
-    echo "De rest na deling van " . $var1 . " gedeeld door " . $var2 . " is : " . ($var1 % $var2);
+        echo "De rest na deling van " . $var1 . " gedeeld door " . $var2 . " is : " . ($var1 % $var2);
     }
 }
 
@@ -100,9 +104,14 @@ function logaritme($var1, $var2) {
         echo "Beide  argumenten moeten  positef zijn voor logaritme berekening";
     } else {
         $uitkomst = log($var2, $var1);
-        
-    echo "Ik moet  " . $var1 . " verheffen tot " . $uitkomst . " om  " . $var2." te krijgen";
+
+        echo "Ik moet  " . $var1 . " verheffen tot " . $uitkomst . " om  " . $var2 . " te krijgen";
     }
+}
+
+function kegeloppervlakte($straal, $hoogte) {
+    $uitkomst = pi() * ( $straal ** 2) +   pi() * $straal  *  ($straal**2 + $hoogte**2 )**0.5  ;
+    echo "De oppervlakte van de kegel met straal  " . $straal . " en hoogte " . $hoogte . " is " . $uitkomst;
 }
 
 ?>
