c14f17ccda532240261e3bcc0e28df68f6574c40
ahtec <gerarddoets@gmail.com>
2017-10-25 15:46:52 +0200

    3e commit

--- weekopdracht1.php
+++ weekopdracht1.php
@@ -1,13 +1,13 @@
 <?php
 
-/* *****************************************************
+/* * ****************************************************
  *  Dit php programma gebruikt 3 parameters
  *  Gerard Doets 25 oktober 2017
  * *****************************************************
   $eerste   dit is het eerste argement van de bewerking
   $tweede   dit is het tweede  argement van de bewerking
   $bewerking = dit is de bewerking tussen de parameters
-**********************************************************
+ * *********************************************************
  */
 $bewerking = $_GET['bewerking'];
 
@@ -32,16 +32,21 @@ switch ($bewerking) {
     case "machtverheffen":
         machtverheffen($eerste, $tweede);
         break;
-    
+
     case "wortel":
         wortel($eerste, $tweede);
         break;
-    
-    
+
+    case "rest":
+        rest($eerste, $tweede);
+        break;
+
+
     default:
         echo "Onbekende bewerking: " . $bewerking;
-        
+
         echo "<br>U kunt gebruiken: optellen aftrekken vermenigvuldigen delen machtverheffen wortel";
+        echo" rest";
         break;
 }
 echo "<br>";
@@ -75,8 +80,12 @@ function machtverheffen($var1, $var2) {
 
 function wortel($var1, $var2) {
     $var3 = 1 / $var2;
-    echo "De ".$var2."macht wortel van " . $var1 .  " is : " . ($var1 ** $var3);
+    echo "De " . $var2 . "macht wortel van " . $var1 . " is : " . ($var1 ** $var3);
 }
 
+function rest($var1, $var2) {
+//    $var3 = 1 / $var2;
+    echo "De rest na deling vav " . $var1 . " gedeeld door " . $var2 . " is : " . ($var1 % $var2);
+}
 
 ?>
