ce9feac6aa94d47b11fc8ed30943e7207d4bc598
ahtec <gerarddoets@gmail.com>
2017-10-31 10:33:25 +0100

    Zeeslag 8

--- zeeslag.php
+++ zeeslag.php
@@ -12,8 +12,8 @@
 //$zee = array();
 //$grooteZee = 4;
 
-$schip1 = new schip(array(array(100, 10), array(100, 11), array(100, 12), array(100, 13), array(100, 14), array(100, 15), array(100, 16), array(100, 17)), "grootschip");
-$schip2 = new schip(array(array(100, 20), array(100, 21), array(100, 22)), "leukschip");
+$schip1 = new schip(array(array(90, 10), array(90, 11), array(90, 12), array(90, 13), array(90, 14), array(90, 15), array(90, 16), array(90, 17)), "De Ruyter");
+$schip2 = new schip(array(array(80, 20), array(80, 21), array(80, 22), array(80, 23), array(80, 24), array(80, 25), array(80, 26), array(80, 27)), "De Kareldoorman");
 $schip3 = new schip(array(array(100, 50), array(100, 51), array(100, 52)), "dedobber");
 $schip4 = new schip(array(array(100, 80), array(100, 51), array(100, 82)), "zeilboot");
 $schip5 = new schip(array(array(100, 11), array(100, 12), array(100, 13)), "roeiboot");
@@ -21,25 +21,19 @@ $alleSchepen = array($schip1, $schip2, $schip3, $schip4, $schip5);
 
 
 schiet(100, 14, $alleSchepen);
-schiet(100, 51, $alleSchepen);
-
-//$tempArray = array();
-//for ($hor = 0; $hor < $grooteZee; $hor++) {
-//    for ($ver = 0; $ver < $grooteZee; $ver++) {
-//        $tempArray[] = [$ver];
-//    }
-//    $zee[] = $tempArray;
-//}
+schiet(80, 26, $alleSchepen);
+schiet(80, 26, $alleSchepen);
+schiet(100, 14, $alleSchepen);
 
 
 
 function schiet($hor, $ver, $param_alleSchepen) {
-    echo "<br> <br>ga schieten op positie : ".$hor ." ".$ver ;
+    echo "<br> <br>Ik schiet op positie : ".$hor ." ".$ver ;
     for ($i = 0; $i < count($param_alleSchepen); $i++) {
 //        $huidigSchip = $param_alleSchepen[$i];
         if ($param_alleSchepen[$i]->geraakt == FALSE) {
 //            $naamSchip = $huidigSchip->naamSchip;
-            echo "<br>Ga schieten op schip: " . $param_alleSchepen[$i]->naamSchip;
+            echo "<br>Ik kijk of ik  " . $param_alleSchepen[$i]->naamSchip . " geraakt hebt";
             $param_alleSchepen[$i]->benIkGeraakt($hor, $ver);
         }
     }
@@ -59,21 +53,30 @@ class schip {
 
     function benIkGeraakt($hor, $ver) {
         $eruit = false;
-
         for ($i = 0; $i < count($this->positie); $i++) {
-//            echo $this->positie[$i][0];
             if ($this->positie[$i][0] == $hor && $this->positie[$i][1] == $ver) {
-                echo  "   ".$this->naamSchip . " boem.... GERAAKT op positie " . $i;
+                echo  " en jawel...".$this->naamSchip . " GERAAKT op positie " . $i;
                 $eruit = TRUE;
                 $this->geraakt = TRUE;
 //                exit();
             }
         }
+        if (!$eruit){
+            echo "  Gelukkig niet geraakt.";
+            
+        }
         return $eruit;
     }
-
 }
 
+
+//$tempArray = array();
+//for ($hor = 0; $hor < $grooteZee; $hor++) {
+//    for ($ver = 0; $ver < $grooteZee; $ver++) {
+//        $tempArray[] = [$ver];
+//    }
+//    $zee[] = $tempArray;
+//}
 
 
 
